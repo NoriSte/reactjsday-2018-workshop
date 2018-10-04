9e283dac6e5105fce9e60d64404ea45fe86fd014
Stefano Magni <nori.ste.magni@gmail.com>
2018-10-04 12:55:39 +0200

    Completed the exercise
    https://labs.reactacademy.io/lab/react-render-props/lesson-2

---
+++ src/components/KeyLogger.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class KeyLogger extends React.Component {
+  state = {
+    lastKeystroke: "",
+    typedText: ""
+  };
+
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  onKeyDown(e) {
+    let lastKeystroke = e.key;
+    let typedText = this.state.typedText;
+    console.log(lastKeystroke);
+    if (
+      lastKeystroke !== "Backspace" &&
+      lastKeystroke !== "Enter" &&
+      lastKeystroke !== "Escape"
+    ) {
+      typedText += lastKeystroke;
+    }
+
+    this.setState({
+      lastKeystroke,
+      typedText
+    });
+  }
+
+  componentDidMount() {
+    this.myRef.current.tabIndex = "0";
+  }
+
+  render() {
+    return (
+      <div ref={this.myRef} onKeyDown={this.onKeyDown}>
+        {this.props.children(this.state)}
+      </div>
+    );
+  }
+}
+
+export { KeyLogger };
--- src/index.js
+++ src/index.js
@@ -6,29 +6,37 @@ import { HOCShowCoords } from "./ShowCoords";
 import { MouseLogger } from "./components/MouseLogger";
 import { MeasureTime } from "./components/MeasureTime";
 import { FetchData } from "./components/FetchData";
+import { KeyLogger } from "./components/KeyLogger";
 
 function App() {
   return (
-    <div className="App">
-      HOC:
-      <HOCShowCoords />
-      <br />
-      <br />
-      Render props:
-      <MouseLogger>
-        {({ x, y }) => (
+    <KeyLogger>
+      {({ lastKeystroke, typedText }) => (
+        <div className="App">
+          HOC:
+          <HOCShowCoords />
+          <br />
+          <br />
+          Render props:
+          <MouseLogger>
+            {({ x, y }) => (
+              <div>
+                x: {x} y: {y}
+              </div>
+            )}
+          </MouseLogger>
+          <MeasureTime>
+            {({ secondsPassed }) => <div>secondsPassed: {secondsPassed}</div>}
+          </MeasureTime>
+          <FetchData apiUrl="https://swapi.co/api/people/1">
+            {({ data }) => <div>data: {data}</div>}
+          </FetchData>
           <div>
-            x: {x} y: {y}
+            lastKeystroke: {lastKeystroke}, typedText: {typedText}
           </div>
-        )}
-      </MouseLogger>
-      <MeasureTime>
-        {({ secondsPassed }) => <div>secondsPassed: {secondsPassed}</div>}
-      </MeasureTime>
-      <FetchData apiUrl="https://swapi.co/api/people/1">
-        {({ data }) => <div>data: {data}</div>}
-      </FetchData>
-    </div>
+        </div>
+      )}
+    </KeyLogger>
   );
 }
 
